fe1700eb7f816a4004d075bf5d1aaf069c94c0c7
laiswally <monomaniarabello@gmail.com>
2025-03-10 20:13:57 -0300

    Formulário funcionando ;)

--- script.js
+++ script.js
@@ -25,20 +25,20 @@ actualTime(); //chamada inicial
 // Dark-mode
 document.addEventListener("DOMContentLoaded", () => {
     const toggleSwitch = document.getElementById("toggleDarkMode");
-    const body = document.body;
-  
+    const body = document.documentElement;
+   
     // Verifica se o usuário já ativou o modo escuro antes
     if (localStorage.getItem("theme") === "dark") {
-      body.classList.add("dark-mode");
+      body.setAttribute("data-theme", "dark");
       toggleSwitch.checked = true;
     }
   
     toggleSwitch.addEventListener("change", () => {
       if (toggleSwitch.checked) {
-        body.classList.add("dark-mode");
+        body.setAttribute("data-theme", "dark");
         localStorage.setItem("theme", "dark");
       } else {
-        body.classList.remove("dark-mode");
+        body.setAttribute("data-theme", "light");
         localStorage.setItem("theme", "light");
       }
     });
@@ -74,10 +74,77 @@ document.addEventListener("DOMContentLoaded", () => {
 //------------------------------------------------------------------------------------------------
 // Form Submit
 
-const form = document.querySelector('form');
+// const form = document.querySelector('form');
 
-form.addEventListener('submit', (evento) => {
-  evento.preventDefault();
-})
+// form.addEventListener('submit', (evento) => {
+//   evento.preventDefault();
+// })
 
+class FormSubmit {
+  constructor(settings) {
+    this.settings = settings;
+    this.form = document.querySelector(settings.form);
+    this.formButton = document.querySelector(settings.button);
+
+    if (this.form) {
+      this.url = this.form.getAttribute("action");
+    }
+    this.sendForm = this.sendForm.bind(this);
+  } 
+
+  displaySuccess() {
+    this.form.innerHTML = this.settings.success;
+  }
+
+  displayError() {
+    this.form.innerHTML = this.settings.error;
+  }
+
+  getFormObject() {
+    const formObject = {};
+    const fields = this.form.querySelectorAll("[name]");
+    fields.forEach((field) => {
+      formObject[field.getAttribute("name")] = field.value;
+    });
+    return formObject;
+  }
+
+  onSubmission(event) {
+    event.preventDefault();
+    event.target.disabled = true;
+    event.target.innerHTML = "Enviando...";
+  }
+
+  async sendForm(event) {
+    try {
+      this.onSubmission(event);
+      await fetch(this.url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(this.getFormObject()),
+      });
+      this.displaySuccess();
+    } catch(error) {
+      this.displayError();
+      throw new Error(error);
+    }
+  }
+
+  // Testa o que estamos fazendo
+  init() {
+    if (this.form) this.formButton.addEventListener("click", this.sendForm);
+    return this;
+  }
+}
+
+const formSubmit = new FormSubmit({
+  form: "[data-form]",
+  button: "[data-button]",
+  success: "<div class='success'><h1>Mensagem Enviada!</h1><i class='bi bi-check-circle-fill'></i></div>",
+  error: "<div class='error'><h1>Não foi possível enviar sua mensagem.</h1><i class='bi bi-emoji-frown-fill'></i></div>",
+});
+formSubmit.init();
 //------------------------------------------------------------------------------------------------
